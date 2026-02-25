8e083575e3d499bc5845e708d308da77c53d8474
SANTOC01 <95682765+SANTOC01@users.noreply.github.com>
2026-02-25 11:43:57 +0100

    Update gipfelstuermerSheetUrl in production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  gipfelstuermerSheetUrl: 'https://script.google.com/macros/s/AKfycbyzRu3ju3OUSfvlKeg1za1lrMmTqZ5V-ytMlcNWGxa1ae8XWpJLgTPfSEBTNF3Rqg02/exec'
+  gipfelstuermerSheetUrl: 'https://script.google.com/macros/s/AKfycbw7zvhtToU381V680N8NzYjykvzOIPR5B6NNY2LRF5vgf4URYsxWBLPefv6TsEHBKAP/exec'
 };
